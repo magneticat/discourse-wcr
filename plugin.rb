e95fd43f595f38b1221ea80cb594727b54be6780
Lorenzo Harry Bassanelli <development@magneticat.com>
2015-11-06 00:25:23 +0100

    First commit

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-wcr
+# about: Window Cleaning Resource customizations.
+# version: 0.0.1
+# authors: MagnetiCat
+
+## General Changes
+register_asset "javascripts/discourse/templates/components/home-logo.hbs"
